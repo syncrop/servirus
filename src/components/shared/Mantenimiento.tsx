daf463dbe3a98d343da4b32db5894485e9562e79
Syncrop <pulidogomez.alberto@gmail.com>
2023-06-02 16:42:03 +0200

    v1.0

--- src/components/shared/Mantenimiento.tsx
+++ src/components/shared/Mantenimiento.tsx
@@ -5,8 +5,8 @@ const Mantenimiento = () => {
         <div className=' flex flex-col items-stretch bg-gray'>
 
             <div className='mt-24 flex flex-col items-center justify-center flex-1'>
-                <div className='md:px-12 md:py-12 my-24 mx-24'>
-                    <img src="mantenimiento.png" />
+                <div className='md:px-12   '>
+                    <img src="/images/mantenimiento.png" />
                     <h1 className='text-4xl md:text-5xl font-bold text-center text-slate-800 tracking-widest mt-4 md:ml-2 drop-shadow-2xl'>Próximamente</h1>
                     <h1 className='text-3xl font-bold text-center text-slate-800 mt-2 drop-shadow-2xl'>En construcción</h1>
                 </div>
